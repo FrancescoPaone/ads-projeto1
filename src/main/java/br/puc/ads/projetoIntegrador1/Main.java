7ce9d3de52ee6d2d83a02806e7c8965ad6289997
Lucas Nogueira Lopes <lucasnog@msn.com>
2024-03-31 18:59:44 -0300

    Alterações fran e lucas projeto 1 31/03/2024

--- src/main/java/br/puc/ads/projetoIntegrador1/Main.java
+++ src/main/java/br/puc/ads/projetoIntegrador1/Main.java
@@ -1,49 +1,125 @@
 package br.puc.ads.projetoIntegrador1;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Boolean continuar = true;
-        String resposta;
+
         Scanner sc = new Scanner(System.in);
         Integer opMenu;
+        Boolean continuar = true;
 
+        String nomePaciente = "";
+        int idadeDoPaciente = 0;
+        String dataDeNascimento = "";
 
-        List<ArrayList> listaPacientes = new ArrayList<>();
-
-        [id, nome, [nomedavacine, lote, dataAplicação]]
-        [[id, nome, vacina], [id, nome, vacina],[id, nome, vacina]]
-
-        //ESTUDAR DO-WHILE E SWITCH CASE.
-
+        String[][] vacinas = new String[40][3];
+        vacinas[0][0] = "1";
 
         do {
 
             System.out.println("Escolha uma opção:");
-            System.out.println("1 - Cadastrar novo Paciente");
+            System.out.println("1 - Cadastro/Informações do Paciente");
             System.out.println("2 - Aplicar vacina");
-            System.out.println("3 - Listar todos os Pacientes");
-            System.out.println("4 - Buscar Paciente pelo Nome");
-            System.out.println("5 - Atualizar Paciente");
-            System.out.println("6 - Excluir Paciente");
-            System.out.println("7 - Sair");
+            System.out.println("3 - Atualizar Paciente");
+            System.out.println("4 - Sair");
 
             opMenu = sc.nextInt();
 
             switch (opMenu) {
-                case 1:
 
-                case 7:
+                case 1:
+                    if (nomePaciente != "") {
+                        System.out.println("------");
+                        System.out.println("Nome do paciente: " + nomePaciente);
+                        System.out.println("Idade do paciente: " + idadeDoPaciente);
+                        System.out.println("Data de nascimento paciente: " + dataDeNascimento);
+                        System.out.println("------");
+                        break;
+                    } else {
+                        System.out.println("Nenhum paciente cadastrado. Cadastrastre o Paciente.");
+                        System.out.println("Digite o nome do paciente:");
+                        nomePaciente = sc.next();
+                        System.out.println("Digite a idade do paciente:");
+                        idadeDoPaciente = sc.nextInt();
+                        System.out.println("Digite a data de nascimento(dd/mm/aaaa) do paciente:");
+                        dataDeNascimento = sc.next();
+                        System.out.println("O paciente: " + nomePaciente + " foi cadastrado com sucesso.");
+                        break;
+                    }
+
+                case 2:
+
+                    if (nomePaciente == ""){
+                        System.out.println("Nenhum paciente cadastrado, por favor, cadastre o paciente primeiro");
+                        break;
+                    }else{
+
+                    System.out.println("Aplicando vacina para " + nomePaciente);
+                    System.out.println("Digite o nome da vacina:");
+                    String nomeVacina = sc.next();
+                    System.out.println("Digite a data da vacina:");
+                    String dataVacina = sc.next();
+                    System.out.println("Digite o lote da vacina:");
+                    String loteVacina = sc.next();
+
+                    int posicaoVazia = -1;
+                    for (int i = 0; i < vacinas.length; i++) {
+                        if (vacinas[i][0] == null) {
+                            posicaoVazia = i;
+                            break;
+                        }
+                    }
+
+                    if (posicaoVazia != -1) {
+                        vacinas[posicaoVazia][0] = nomeVacina;
+                        vacinas[posicaoVazia][0] = dataVacina;
+                        vacinas[posicaoVazia][0] = loteVacina;
+                        System.out.println("Vacina " + nomeVacina + " aplicada com sucesso ");
+                        break;
+                    }
+                        }
+                case 3:
+
+                    if (nomePaciente == ""){
+                        System.out.println("Nenhum paciente cadastrado, por favor, cadastre o paciente primeiro");
+                        break;
+                    }else{
+                        System.out.println("O que deseja alterar?");
+                        System.out.println("1 - Nome");
+                        System.out.println("2 - Idade");
+                        System.out.println("3 - Data de nascimento (dd/mm/aaaa)");
+                        int opMod = sc.nextInt();
+                        switch (opMod) {
+                            case 1:
+                                System.out.println("Digite o novo nome do paciente:");
+                                nomePaciente = sc.next();
+                                System.out.println("Nome modificada com sucesso.");
+                                break;
+
+                            case 2:
+                                System.out.println("Digite a nova idade do paciente:");
+                                idadeDoPaciente = sc.nextInt();
+                                System.out.println("Idade modificada com sucesso.");
+                                break;
+
+
+                            case 3:
+                                System.out.println("Digite a nova data de nascimento(dd/mm/aaaa) do paciente:");
+                                dataDeNascimento = sc.next();
+                                System.out.println("Data de nascimento modificada com sucesso.");
+                                break;
+                        }
+
+                    }
+
+                case 4:
                     System.out.println("Até logo.");
                     continuar = false;
             }
 
         } while (continuar);
-
     }
 }
